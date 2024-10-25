3bf6e65b7c494e8f6e757be89c0e61756d7bd26d
Ronald Casili <nvlled@gmail.com>
2024-10-25 13:26:59 +0800

    Use tokenizer directly
    ...since net/html parser always wraps the output in
    <html><body>$content</body></html> for
    some strange. This behaviour can't be configured,
    so use tokenizer directly.

--- format.go
+++ format.go
@@ -4,12 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"iter"
-	"log"
 	"strings"
 	"unicode"
 
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func String(htmlStr string) string {
@@ -19,51 +18,90 @@ func String(htmlStr string) string {
 }
 
 func Output(htmlStr string, w io.Writer) {
-	doc, err := html.Parse(strings.NewReader(htmlStr))
-	if err != nil {
-		log.Fatal(err)
+	z := html.NewTokenizer(strings.NewReader(htmlStr))
+	depth := 0
+	pool := &nodePool{}
+
+	var parent *html.Node = new(html.Node)
+	var token *html.Token = new(html.Token)
+
+	createNode := func(token *html.Token, ntype html.NodeType, parent *html.Node) *html.Node {
+		node := pool.get()
+		node.Type = ntype
+		node.DataAtom = token.DataAtom
+		node.Data = token.Data
+		node.Attr = token.Attr
+		if parent != nil {
+			parent.AppendChild(node)
+		}
+		return node
 	}
 
-	formatHTML(doc, w, 0)
-}
-
-func formatHTML(n *html.Node, w io.Writer, level int) {
-	indent := strings.Repeat(" ", level*2)
-
-	for c := range iterateNonEmptyChildren(n) {
-		inline := isInline(c)
-
-		if c.Type == html.ElementNode {
-			if isInline(c.PrevSibling) && !isInline(c) {
-				fmt.Fprint(w, "\n")
+	initToken := func(tt html.TokenType, t *html.Token) *html.Token {
+		t.Type = tt
+		t.Attr = t.Attr[:0]
+		switch tt {
+		case html.TextToken, html.CommentToken, html.DoctypeToken:
+			t.Data = string(z.Text())
+		case html.StartTagToken, html.SelfClosingTagToken, html.EndTagToken:
+			name, moreAttr := z.TagName()
+			for moreAttr {
+				var key, val []byte
+				key, val, moreAttr = z.TagAttr()
+				t.Attr = append(t.Attr, html.Attribute{
+					Key: atom.String(key),
+					Val: string(val),
+				})
 			}
-			if !isInline(c.PrevSibling) || !isInline(c) {
-				fmt.Fprint(w, indent)
-			}
-
-			fmt.Fprintf(w, "<%s", c.Data)
-			for _, attr := range c.Attr {
-				fmt.Fprintf(w, ` %s="%s"`, attr.Key, attr.Val)
-			}
-
-			fmt.Fprint(w, ">")
-			if !inline {
-				fmt.Fprint(w, "\n")
-			}
-
-		} else if c.Type == html.TextNode {
-			if isInline(n) {
-				fmt.Fprintf(w, c.Data)
+			if a := atom.Lookup(name); a != 0 {
+				t.DataAtom, t.Data = a, a.String()
 			} else {
-				leftSpace := unicode.IsSpace(rune(c.Data[0]))
-				rightSpace := unicode.IsSpace(rune(c.Data[len(c.Data)-1]))
+				t.DataAtom, t.Data = 0, string(name)
+			}
+		}
+		return t
+	}
 
-				if leftSpace && isInline(c.PrevSibling) {
+loop:
+	for {
+		tt := z.Next()
+		indent := strings.Repeat(" ", depth*2)
+
+		// note: node.NextSibling will be always null
+		// since this is "one-pass" iteration and
+		// so the code won't know in advance what
+		// the following nodes are.
+
+		switch tt {
+		case html.ErrorToken:
+			if z.Err() == io.EOF {
+				break loop
+			} else {
+				panic(z.Err())
+			}
+
+		case html.TextToken:
+			node := createNode(initToken(tt, token), html.TextNode, nil)
+			if !strings.ContainsFunc(node.Data, isNotSpace) {
+				// skip text nodes with whitespace only
+				continue
+			}
+
+			if parent != nil {
+				parent.AppendChild(node)
+			}
+
+			if isInline(node.Parent) {
+				fmt.Fprintf(w, node.Data)
+			} else {
+				leftSpace := unicode.IsSpace(rune(node.Data[0]))
+
+				if leftSpace && isInline(node.PrevSibling) {
 					fmt.Fprint(w, " ")
 				}
 
-				for line := range getLines(strings.TrimSpace(c.Data)) {
-					if !isInline(c.PrevSibling) || !isInline(c) {
+				for line := range getLines(strings.TrimSpace(node.Data)) {
+					if !isInline(node.PrevSibling) || !isInline(node) {
 						fmt.Fprint(w, indent)
 					}
 					if len(line) > 0 {
@@ -76,62 +114,66 @@ func formatHTML(n *html.Node, w io.Writer, level int) {
 						}
 					}
 				}
+			}
 
-				if rightSpace && isInline(c.NextSibling) {
+		case html.SelfClosingTagToken, html.StartTagToken:
+			node := createNode(initToken(tt, token), html.ElementNode, parent)
+
+			if tt == html.StartTagToken {
+				parent = node
+				depth++
+			}
+
+			if isInline(node) && node.PrevSibling != nil {
+				p := node.PrevSibling
+				rightSpace := unicode.IsSpace(rune(p.Data[len(p.Data)-1]))
+				if rightSpace {
 					fmt.Fprint(w, " ")
 				}
 			}
-		} else if c.Type == html.CommentNode {
-			if isInline(c) {
-				fmt.Fprintf(w, "<!--%s-->", c.Data)
-			} else {
-				if isInline(c.PrevSibling) {
-					fmt.Fprint(w, "\n")
+			if isInline(node.PrevSibling) && !isInline(node) {
+				fmt.Fprint(w, "\n")
+			}
+			if !isInline(node.PrevSibling) || !isInline(node) {
+				fmt.Fprint(w, indent)
+			}
+
+			fmt.Fprintf(w, "<%s", node.Data)
+
+			for _, attr := range node.Attr {
+				fmt.Fprintf(w, ` %s="%s"`, attr.Key, attr.Val)
+			}
+
+			fmt.Fprint(w, ">")
+			if !isInline(node) {
+				fmt.Fprint(w, "\n")
+			}
+
+		case html.EndTagToken:
+			node := parent
+			parent = node.Parent
+			if depth > 0 {
+				depth--
+			}
+			indent := strings.Repeat(" ", depth*2)
+
+			if !isVoid(node) {
+				if isInline(node) {
+					fmt.Fprintf(w, "</%s>", node.Data)
+				} else {
+					if isInline(node.LastChild) {
+						fmt.Fprint(w, "\n")
+					}
+					fmt.Fprintf(w, "%s</%s>\n", indent, strings.TrimSpace(node.Data))
 				}
-				fmt.Fprintf(w, "%s<!--%s-->\n", indent, c.Data)
 			}
-		} else if c.Type == html.DoctypeNode {
-			fmt.Fprintf(w, "<!DOCTYPE %s", c.Data)
-			for _, attr := range c.Attr {
-				if attr.Key == "public" {
-					fmt.Fprint(w, " PUBLIC")
-				}
-				fmt.Fprintf(w, ` "%v"`, attr.Val)
+			for c := node.FirstChild; c != nil; c = c.NextSibling {
+				node.RemoveChild(c)
+				pool.free(c)
 			}
-			fmt.Fprint(w, ">\n")
-		}
 
-		formatHTML(c, w, level+1)
-
-		if c.Type == html.ElementNode && !isVoid(c) {
-			if inline {
-				fmt.Fprintf(w, "</%s>", c.Data)
-			} else {
-				if isInline(c.LastChild) {
-					fmt.Fprint(w, "\n")
-				}
-				fmt.Fprintf(w, "%s</%s>\n", indent, strings.TrimSpace(c.Data))
-			}
-		}
-	}
-
-}
-
-// skips text nodes with only whitespaces
-func iterateNonEmptyChildren(node *html.Node) iter.Seq[*html.Node] {
-	return func(yield func(*html.Node) bool) {
-		child := node.FirstChild
-		for child != nil {
-			isEmptyStr := child.Type == html.TextNode && strings.TrimSpace(child.Data) == ""
-
-			if isEmptyStr {
-				next := child.NextSibling
-				node.RemoveChild(child)
-				child = next
-				continue
-			}
-			yield(child)
-			child = child.NextSibling
+		case html.CommentToken, html.DoctypeToken:
+			fmt.Fprintf(w, "%s%s%s", indent, string(z.Raw()), "\n")
 		}
 	}
 }
